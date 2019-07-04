583fd9411a6de3dd8846225f87db6e3cd047a1d6
Heather Zurek <heather.zurek@gmail.com>
2019-07-04 09:14:38 -0700

    add header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import Header from './Header';
-// import TicketList from './TicketList';
-// import { Switch, Route } from 'react-router-dom';
-// import NewTicketForm from './NewTicketForm';
-
-
 
 function App() {
   return (
     <div>
       <Header />
-      <Switch>
-        // <Route exact path='/' component={TicketList} />
-        // <Route path='/newticket' component={NewTicketForm} />
-
-      </Switch>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Header(){
+  return (
+    <div>
+      <h1>Twin Peaks Coffee Roasters</h1>
+      <h3><em>A damn fine cup of coffee</em></h3>
+    </div>
+  );
+}
+
+export default Header;
